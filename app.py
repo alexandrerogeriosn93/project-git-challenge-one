1dd34d62b39f73e559ab28b96dd3f3c1deece966
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-02-09 15:08:32 -0300

    feat: generating even numbers list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import random
 
-list_randon = [random.randrange(1, 100, 1) for x in range(10)]
-
+list_randon = sorted([random.randrange(1, 100, 1) for x in range(10)])
+list_even = [int(x) for x in list_randon if x % 2 == 0]
