c5323230e5d726505be5ff70ba69a4fce48e80cc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-02-09 15:10:46 -0300

    feat: generating odd numbers list

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
 
-list_randon = sorted([random.randrange(1, 100, 1) for x in range(10)])
+list_randon = sorted([random.randrange(1, 100, 1) for _ in range(10)])
 list_even = [int(x) for x in list_randon if x % 2 == 0]
+list_odd = [int(x) for x in list_randon if x % 2 != 0]
